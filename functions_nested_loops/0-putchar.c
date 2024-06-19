2b897d4eeddc9f5f94189efc76a07cd899be54b3
William Alvarado Pereira <jdlejosj@gmail.com>
2024-06-19 09:37:29 -0400

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,7 @@
-#include <main.h>
+#include <unistd.h>
 
-/**
- * main - Prints "_putchar" followed by a new line.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	_putchar('_')
-	_putchar('p')
-	_putchar('u')
-	_putchar('t')
-	_putchar('c')
-	_putchar('h')
-	_putchar('a')
-	_putchar('r')
-	_putchar('\n')
-
-	return (0);
+int main() {
+    char *message = "_putchar\n";
+    write(1, message, 9); // Using write to output the message to stdout
+    return 0;
 }
