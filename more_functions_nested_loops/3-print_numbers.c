a8772b7a1fa075934728793ffeafbf3690bc4aa1
William Alvarado Pereira <jdlejosj@gmail.com>
2024-06-20 13:28:02 -0400

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
- * Description: print 0 to 9
- * Return: returns 0
+ * print_most_numbers - Prints 0-9
+ * Returns: void
  */
+
 void print_most_numbers(void)
 {
 	char c;
