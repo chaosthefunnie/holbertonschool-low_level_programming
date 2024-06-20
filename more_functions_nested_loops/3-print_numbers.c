7e75daad81b227a042124dd96ecbcc1ef4558492
William Alvarado Pereira <jdlejosj@gmail.com>
2024-06-20 13:26:30 -0400

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ * Description: print 0 to 9
+ * Return: returns 0
  */
 void print_most_numbers(void)
 {
-	int num;
+	char c;
 
-	for (num = 0; num <= 9; num++)
+	for (c = '0';  c <='9'; c++)
 	{
-		if (num != 2 && num != 4)
-			_putchar((num % 10) + '0');
+	_putchar(c);
 	}
-
 	_putchar('\n');
 }
