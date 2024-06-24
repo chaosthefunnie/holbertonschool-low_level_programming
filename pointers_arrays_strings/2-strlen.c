4e2362444e515fa0c06b3f7c65fc667e81d3522c
William Alvarado Pereira <jdlejosj@gmail.com>
2024-06-24 10:21:50 -0400

    2-2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,11 @@
  * _strlen - returns to string.
  * @str: the string of length.
  *
- * Return: the lenghth of @str.
+ * Return: the length of @str.
  */
 int _strlen(char *str)
 {
-	size_t lenght = 0;
+	size_t length = 0;
 
 	while (*str++)
 		length++;
